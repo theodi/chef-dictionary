1ea6ff5a5cf716f50f0bf428688e9e3916bff30d
pikesley <github@orgraphone.org>
2013-08-01 11:50:18 +0100

    Metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'dictionary'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures dictionary'
+maintainer       'The Open Data Institute'
+maintainer_email 'tech@theodi.org'
+license          'MIT'
+description      'Installs /usr/share/dict/words'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
